f9f1092213b2790b007e9426f62989598a741d7d
Tiffany <tiffanyisaprogrammer@gmail.com>
2026-07-29 10:44:38 +0700

    i did it

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from EC2! I Did it!'));
+app.get('/', (req, res) => res.send('Hello from EC2! I Did it! just with push'));
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
