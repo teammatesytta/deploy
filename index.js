4d46fe98793db0c22a76dac572af1376ef55da7b
Tiffany <tiffanyisaprogrammer@gmail.com>
2026-07-29 08:53:28 +0700

    proud

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from EC2!'));
+app.get('/', (req, res) => res.send('Hello from EC2! I Did it!'));
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
